7a9a26e712824d5aec9ac7d7d2d8a8a7cad74fed
ANTONIO YEE <yee.antonio@gmail.com>
2015-05-10 14:00:41 -0500

    version tablet 10"

--- app/src/main/java/mx/antonioyee/musicplayer/ManagerActivity.java
+++ app/src/main/java/mx/antonioyee/musicplayer/ManagerActivity.java
@@ -3,24 +3,37 @@ package mx.antonioyee.musicplayer;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class ManagerActivity extends ActionBarActivity implements ListMusicFragment.OnFragmentInteractionListener {
 
     private FragmentManager fragmentManager;
+    private String resourceType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manager);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_manager, null);
+        resourceType = view.getTag().toString();
+
+        setContentView(view);
 
         fragmentManager = getSupportFragmentManager();
 
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, ListMusicFragment.newInstance(Music.getSongs(getApplicationContext())))
-                .commit();
+        if ( resourceType.equals("sw600dp") ){
+            fragmentManager.beginTransaction()
+                    .replace(R.id.containerLeft, ListMusicFragment.newInstance(Music.getSongs(getApplicationContext())))
+                    .commit();
+        }else{
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, ListMusicFragment.newInstance(Music.getSongs(getApplicationContext())))
+                    .commit();
+        }
     }
 
     @Override
@@ -47,11 +60,15 @@ public class ManagerActivity extends ActionBarActivity implements ListMusicFragm
 
     @Override
     public void onMusicSelect(int position) {
-
-        fragmentManager.beginTransaction()
-                .replace(R.id.container,PlayerFragment.newInstance(position))
-                .addToBackStack(null)
-                .commit();
-
+        if ( resourceType.equals("sw600dp") ){
+            fragmentManager.beginTransaction()
+                    .replace(R.id.containerRight,PlayerFragment.newInstance(position))
+                    .commit();
+        }else{
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container,PlayerFragment.newInstance(position))
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
